8da23552682bc216546aa48cb10861119f386a81
Bahar <baharragayeva@gmail.com>
2024-04-04 13:53:35 +0400

    finall

--- Business/Validations/AdvertisementBannerValidator.cs
+++ Business/Validations/AdvertisementBannerValidator.cs
@@ -7,9 +7,9 @@ namespace Business.Validations
     {
         public AdvertisementBannerValidator()
         {
-            RuleFor(x => x.Title).NotEmpty().WithMessage("dhjjjjjjjjjjfk")
+            RuleFor(x => x.Title).NotEmpty().WithMessage(ValidationMessages.ValidationMessages.CanNotBeEmpty)
                                  .MaximumLength(50).WithMessage(ValidationMessages.ValidationMessages.HasMaxLength);
-            RuleFor(x => x.Description).NotEmpty().WithMessage("sfsfjjjjjjjjjjfk")
+            RuleFor(x => x.Description).NotEmpty().WithMessage(ValidationMessages.ValidationMessages.CanNotBeEmpty)
                                        .Length(10, 200).WithMessage(ValidationMessages.ValidationMessages.HasLength);
             RuleFor(x => x.ImgPath).NotEmpty().WithMessage(ValidationMessages.ValidationMessages.FileCanNotBeEmpty);
             RuleFor(x => x.Discount).NotEmpty().WithMessage(ValidationMessages.ValidationMessages.CanNotBeEmpty);
